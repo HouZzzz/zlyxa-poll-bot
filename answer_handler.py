9e8f644f2323ce4bbcebf0bc15723a02caef947d
HouZzzz <hozzchef@gmail.com>
2023-03-10 13:39:04 +0000

    answer handling improvement

--- answer_handler.py
+++ answer_handler.py
@@ -1,56 +1,20 @@
 import questions_container
 def handle_answer(answer,question_id):
     possible_answers = questions_container.answers[question_id]
+    possible_emotions = questions_container.emotions[question_id];
     print("possible answers:", possible_answers)
 
+    # incorrect answer
     if answer not in possible_answers:
-        return "Выбирай номрально😡";
-    else:
-        if question_id == 0:
-            return ""
+        return "Выбирай номрально😡|-1";
 
-        elif question_id == 1:
-            if answer == possible_answers[0]:
-                return "бош малявка"
-            else:
-                return ""
+    # question don't have possible emotions
+    if (len(possible_emotions) == 0):
+        return ""
 
-        elif question_id == 2:
-            if answer == possible_answers[1]:
-                return "хавхваваавххавх лох|1"
-            else:
-                return ""
+    # find and return emotion
+    for i in range(len(possible_answers)):
+        if (answer == possible_answers[i]):
+            return possible_emotions[i]
 
-        elif question_id == 3:
-            if answer == possible_answers[2]:
-                return "пиздабол"
-            else:
-                return ""
-
-        elif question_id == 5:
-            if answer == possible_answers[1]:
-                return "|1"
-            else:
-                return ""
-        elif question_id == 6:
-            return "|1";
-        elif question_id == 8:
-            if answer == possible_answers[0]:
-                return "🥰"
-            else:
-                return "<a href='https://www.youtube.com/@zlyxa/featured'>жду тебя мой пупсик</a>"
-
-        elif question_id == 9:
-            if answer == possible_answers[0]:
-                return "Респект"
-            else:
-                return "Позорище🤮"
-
-        elif question_id == 9:
-            if answer == possible_answers[2]:
-                return "<a href='https://youtu.be/CzBP3i_7KYg'>узнать</a>"
-            else:
-                return ""
-
-        else:
-            return "";
+    return "";
