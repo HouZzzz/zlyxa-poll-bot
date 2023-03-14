cb01b6454dcf587eb680dc43693c740b3402a5df
HouZzzz <hozzchef@gmail.com>
2023-03-14 12:03:00 +0000

    fastapi

--- main.py
+++ main.py
@@ -8,12 +8,15 @@ import questions_container
 
 # server for vercel
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World!"}
+
+uvicorn.run(app, host="", port=int(os.environ.get("PORT",8080)), log_level="info")
 
 
 token = os.environ.get('TOKEN')
