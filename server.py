826e346cc028a7e948ecc980185754d79887e038
HouZzzz <hozzchef@gmail.com>
2023-03-14 11:50:17 +0000

    new server

--- server.py
+++ server.py
@@ -1,10 +1,7 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from fastapi import FastAPI
 
-#just for vercel
-class MyHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        message = 'Hello, world!'
-        self.wfile.write(bytes(message, 'utf8'))
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World!"}
