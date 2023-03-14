78a21b71de8a5fea00facc8f2bd6306bc5b597f1
HouZzzz <hozzchef@gmail.com>
2023-03-14 10:53:48 +0000

    another doc code

--- main.py
+++ main.py
@@ -6,16 +6,21 @@ import telebot
 import answer_handler
 import questions_container
 
-from http.server import BaseHTTPRequestHandler, HTTPServer
-
 #just for vercel
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib import parse
 class handler(BaseHTTPRequestHandler):
-
     def do_GET(self):
+        s = self.path
+        dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
+        if "name" in dic:
+            message = "Hello, " + dic["name"] + "!"
+        else:
+            message = "Hello, stranger!"
+        self.wfile.write(message.encode())
         return
 
 
